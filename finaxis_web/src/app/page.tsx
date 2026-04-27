4b71d87c0714aa54ffa9047ce414da1403e45ad2
Raphasha27 <raphashakoketso69@gmail.com>
2026-04-28 00:45:09 +0200

    fix: resolve ESLint 'any' type error causing Vercel build failure

--- finaxis_web/src/app/page.tsx
+++ finaxis_web/src/app/page.tsx
@@ -541,6 +541,6 @@ export default function FinAxisWeb() {
 }
 
 // Activity Icon Component Polyfill since lucide-react import list was getting long
-function Activity(props: any) {
+function Activity(props: React.SVGProps<SVGSVGElement>) {
     return <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="22 12 18 12 15 21 9 3 6 12 2 12"></polyline></svg>;
 }
